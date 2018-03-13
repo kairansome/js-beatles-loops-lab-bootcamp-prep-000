3076fac1ceff6f188bc41ef9cf718bb139f525f5
Kai Ransome <ransomekai@gmail.com>
2018-03-13 00:04:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles (number) {
 var array = []
   do { array.push('I love the Beatles!') }
-  while (number)< 15
+  while ((number)< 15)
   
 }
